260b9d282d7d701eb8aa37ed50595e8a964b8bb5
George Fotopoulos <giorgos.fotopoulos7@gmail.com>
2019-04-09 23:28:21 +0300

    Added more methods and Administrator class.

--- R3/src/Dentist.java
+++ R3/src/Dentist.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Dentist {
     public String firstName, lastName, telNo, email, exerciseLicense, universityAttended, infirmaryLocation, timeOfExperience, password, dentistID;
     public HashMap<String, String> credentials = new HashMap();
+    public ArrayList<String> appointmentList = new ArrayList<>();
 
     public Dentist(String firstName, String lastName, String telNo, String email, String exerciseLicense, String universityAttended, String infirmaryLocation, String timeOfExperience, String password, String dentistID) {
         this.firstName = firstName;
@@ -22,7 +25,7 @@ public class Dentist {
      * @return The Hash Map containing that dentist's Username and Password.
      */
     public HashMap<String, String> register(Dentist dentist) {
-        return credentials;
+        return dentist.credentials;
     }
 
     /**
@@ -34,4 +37,101 @@ public class Dentist {
         System.out.println();
     }
 
+    /**
+     * This method allows the dentist to create a new tab for a specific patient using his AMKA number as a guide.
+     *
+     * @param AMKA This indicates the AMKA of the client for whom the dentist wants to create a tab for future use.
+     */
+    public void createPatientCard(String AMKA) {
+
+    }
+
+    public ArrayList<String> viewAppointmentList() {
+        return appointmentList;
+    }
+
+    /**
+     * The following methods allow modifications for one particular dentist attribute and replace the current value
+     * with the one given as a parameter.
+     *
+     * @param data The new value of the attribute.
+     */
+    public void setFirstName(String data) {
+        this.firstName = data;
+    }
+
+    public void setLastName(String data) {
+        this.lastName = data;
+    }
+
+    public void setTelNo(String data) {
+        this.telNo = data;
+    }
+
+    public void setEmail(String data) {
+        this.email = data;
+    }
+
+    public void setExerciseLicense(String data) {
+        this.exerciseLicense = data;
+    }
+
+    public void setUniversityAttended(String data) {
+        this.universityAttended = data;
+    }
+
+    public void setInfirmaryLocation(String data) {
+        this.infirmaryLocation = data;
+    }
+
+    public void setTimeOfExperience(String data) {
+        this.timeOfExperience = data;
+    }
+
+    public void setPassword(String data) {
+        this.password = data;
+    }
+
+    public void modifyData() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("1. Change first name\n2. Change last name\n3.Change telephone number\n" +
+                "4. Change e-mail address\n5. Change exercise license details\n6. Change university of studies\n" +
+                "7. Change infirmary location\n8. Change work experience time\n9. Change Password\n");
+        int choice = in.nextInt();
+        while(choice<1 || choice > 9) {
+            System.out.println("Invalid input, please choose a value between 1 and 9: ");
+            in.nextInt();
+        }
+        switch (choice) {
+            case 1:
+                this.setFirstName(in.nextLine());
+                break;
+            case 2:
+                this.setLastName(in.nextLine());
+                break;
+            case 3:
+                this.setTelNo(in.nextLine());
+                break;
+            case 4:
+                this.setEmail(in.nextLine());
+                break;
+            case 5:
+                this.setExerciseLicense(in.nextLine());
+                break;
+            case 6:
+                this.setUniversityAttended(in.nextLine());
+                break;
+            case 7:
+                this.setInfirmaryLocation(in.nextLine());
+                break;
+            case 8:
+                this.setTimeOfExperience(in.nextLine());
+                break;
+            case 9:
+                this.setPassword(in.nextLine());
+                break;
+            default:
+                break;
+        }
+    }
 }
