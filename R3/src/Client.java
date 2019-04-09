aafc8b2a90d2958ead99b3b529b846465c63cf5d
George Fotopoulos <giorgos.fotopoulos7@gmail.com>
2019-04-09 20:21:37 +0300

    Default Dentist, Client classes.

---
+++ R3/src/Client.java
@@ -0,0 +1,60 @@
+public class Client {
+    private String firstName, lastName, address, telNo, email, AMKA;
+
+    public Client(String firstName, String lastName, String address, String telNo, String email, String AMKA) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.telNo = telNo;
+        this.email = email;
+        this.AMKA = AMKA;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAMKA() {
+        return AMKA;
+    }
+
+    public void setAMKA(String AMKA) {
+        this.AMKA = AMKA;
+    }
+}
