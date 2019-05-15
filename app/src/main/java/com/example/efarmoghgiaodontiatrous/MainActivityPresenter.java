60c1d2c86a76996c5a0f48523cdd0a8408dc9d25
geopatrikis <geopatrikis12@gmail.com>
2019-05-16 01:35:05 +0300

    TODO

---
+++ app/src/main/java/com/example/efarmoghgiaodontiatrous/MainActivityPresenter.java
@@ -0,0 +1,15 @@
+package com.example.efarmoghgiaodontiatrous;
+
+public class MainActivityPresenter {
+    private MainActivityView view;
+
+    /**
+     * Αρχικοποιεί τον Presenter.
+     * @param view Ένα instance του view
+     */
+    public MainActivityPresenter(MainActivityView view)
+    {
+        this.view = view;
+    }
+
+}
