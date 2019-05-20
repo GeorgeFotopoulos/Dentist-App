a375acae3774b487931a0ef7a7b4e1da06d2840c
George Fotopoulos <giorgos.fotopoulos7@gmail.com>
2019-05-20 13:01:30 +0300

    DentistDAO fixes

--- app/src/main/java/com/example/efarmoghgiaodontiatrous/DentistDAOMemory.java
+++ app/src/main/java/com/example/efarmoghgiaodontiatrous/DentistDAOMemory.java
@@ -6,22 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 class DentistDAOMemory implements DentistDAO {
-    protected static List<Dentist> entities = new ArrayList<Dentist>();
+    protected static List<Dentist> entities = new ArrayList<>();
 
     @Override
-    public Dentist find(String DentistID) {
-        for(Dentist dentist : entities) {
-            if (dentist.getID() == DentistID ) {
+    public Dentist find(String dentistId) {
+        for (Dentist dentist : entities) {
+            if (dentist.getID().equals(dentistId)) {
                 return dentist;
             }
         }
-
         return null;
     }
 
     @Override
     public void save(Dentist entity) {
-        if (! entities.contains(entity)) {
+        if (!entities.contains(entity)) {
             entities.add(entity);
         }
     }
@@ -32,31 +31,30 @@ class DentistDAOMemory implements DentistDAO {
     }
 
     @Override
-    public List<Dentist> findByName(String lastName,String firstName) {
-        List<Dentist> output= new ArrayList<Dentist>();
-        if(lastName==null||lastName.equals("")) {
+    public List<Dentist> findByName(String lastName, String firstName) {
+        List<Dentist> output = new ArrayList<>();
+        if (lastName == null || lastName.equals("")) {
             return null;
-        }
-        else if(firstName==null||firstName.equals("")){
-            for(Dentist dentist : entities) {
+        } else if (firstName == null || firstName.equals("")) {
+            for (Dentist dentist : entities) {
                 if (dentist.getLastName().equals(lastName)) {
                     output.add(dentist);
                 }
             }
-        }
-        else{
-            for(Dentist dentist : entities) {
+        } else {
+            for (Dentist dentist : entities) {
                 if (dentist.getLastName().equals(lastName)) {
-                    if(dentist.getFirstName().equals(firstName))
-                      output.add(dentist);
+                    if (dentist.getFirstName().equals(firstName)) {
+                        output.add(dentist);
+                    }
                 }
             }
         }
-        return new ArrayList<Dentist>(entities);
+        return new ArrayList<>(entities);
     }
 
     @Override
     public List<Dentist> findAll() {
-        return new ArrayList<Dentist>(entities);
+        return new ArrayList<>(entities);
     }
 }
