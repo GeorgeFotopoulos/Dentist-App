9e56a0115c33d15cda06c3094d6792bfcf6422f8
geopatrikis <geopatrikis12@gmail.com>
2019-05-22 15:01:18 +0300

    TODO

--- app/src/main/java/com/example/efarmoghgiaodontiatrous/dao/Initializer.java
+++ app/src/main/java/com/example/efarmoghgiaodontiatrous/dao/Initializer.java
@@ -106,9 +106,14 @@ public abstract class Initializer {
         clientDAO.save(new Client("Giorgos", "Ntymenos", "+30 698 468 9640", "giorgos.nty@gmail.com", "17099600037"));
 
         AppointmentDAO appointmentDAO = getAppointmentDAO();
-        appointmentDAO.save(new Appointment("George", "Patrikis", "+30 698 000 0000", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 15, 0));
+        appointmentDAO.save(new Appointment("George", "Patrikis", "+30 698 000 0000", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 15, 00));
         appointmentDAO.save(new Appointment("Leuterakis", "Patrikis", "+30 698 111 1111", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 15, 30));
-        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 16, 0));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 16, 00));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 17, 00));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 13, 00));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 12, 00));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 11, 00));
+        appointmentDAO.save(new Appointment("Georgios", "Patrikis", "+30 698 222 2222", dentistDAO.find("6"), new SimpleCalendar(2019, 28, 5), 10, 00));
 
         VisitDAO visitDAO = getVisitDAO();
         Set<Service> services1 = new HashSet<>();
