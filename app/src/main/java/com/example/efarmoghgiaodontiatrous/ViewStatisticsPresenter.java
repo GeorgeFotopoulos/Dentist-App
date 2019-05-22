faef772e9de6704bae80efafd9595ddbb7661560
George Fotopoulos <giorgos.fotopoulos7@gmail.com>
2019-05-23 01:07:10 +0300

    UI Fixes

--- app/src/main/java/com/example/efarmoghgiaodontiatrous/ViewStatisticsPresenter.java
+++ app/src/main/java/com/example/efarmoghgiaodontiatrous/ViewStatisticsPresenter.java
@@ -10,16 +10,16 @@ import com.example.efarmoghgiaodontiatrous.memorydao.VisitDAOMemory;
 public class ViewStatisticsPresenter {
     ViewStatisticsView view;
 
-    public ViewStatisticsPresenter(ViewStatisticsView view){
-        this.view=view;
+    public ViewStatisticsPresenter(ViewStatisticsView view) {
+        this.view = view;
     }
 
-    public String onWelcome(String ID){
+    public String onWelcome(String ID) {
         DentistDAOMemory ddao = new DentistDAOMemory();
-        return "Welcome Dr. "+ ddao.find(ID).getLastName()+"! \n Here you can see the operations you made:";
+        return "Welcome Dr. " + ddao.find(ID).getLastName() + "!\nBelow is the list with all the successful operations you have made:";
     }
 
-    public String onStats(String ID){
+    public String onStats(String ID) {
         DentistDAOMemory ddao = new DentistDAOMemory();
         VisitDAOMemory vdao = new VisitDAOMemory();
         ServiceDAOMemory sdao = new ServiceDAOMemory();
@@ -27,22 +27,22 @@ public class ViewStatisticsPresenter {
         String result = "";
         int times;
         boolean has = false;
-        for(Service serv : sdao.findAll()){
+        for (Service serv : sdao.findAll()) {
             times = 0;
-            for(Visit visit : vdao.findAll()){
-                if(dentist.equals(visit.getDentist()) && visit.getServices().contains(new Service(serv.getServiceName(),serv.getServiceID()))){
+            for (Visit visit : vdao.findAll()) {
+                if (dentist.equals(visit.getDentist()) && visit.getServices().contains(new Service(serv.getServiceName(), serv.getServiceID()))) {
                     has = true;
                     times++;
                 }
             }
-            if(times == 1){
-                result = result + serv.getServiceName() + ": " + times + " successful operation! \n";
-            }else if(times != 0){
-                result = result + serv.getServiceName() + ": " + times + " successful operations! \n";
+            if (times == 1) {
+                result = result + serv.getServiceName() + ": " + times + " successful operation\n";
+            } else if (times != 0) {
+                result = result + serv.getServiceName() + ": " + times + " successful operations\n";
             }
         }
-        if(!has){
-            result = "No operations yet :(";
+        if (!has) {
+            result = "No successful operations yet!";
         }
         return result;
     }
