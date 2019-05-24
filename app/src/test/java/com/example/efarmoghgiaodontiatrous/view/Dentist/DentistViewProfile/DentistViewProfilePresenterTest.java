f9afa3e5e1219da027892c1f42d4d11cf6ea19e0
geopatrikis <geopatrikis12@gmail.com>
2019-05-24 19:37:53 +0300

    TODO

--- app/src/test/java/com/example/efarmoghgiaodontiatrous/view/Dentist/DentistViewProfile/DentistViewProfilePresenterTest.java
+++ app/src/test/java/com/example/efarmoghgiaodontiatrous/view/Dentist/DentistViewProfile/DentistViewProfilePresenterTest.java
@@ -1,4 +1,67 @@
 package com.example.efarmoghgiaodontiatrous.view.Dentist.DentistViewProfile;
 
+import com.example.efarmoghgiaodontiatrous.dao.AppointmentDAO;
+import com.example.efarmoghgiaodontiatrous.dao.ClientDAO;
+import com.example.efarmoghgiaodontiatrous.dao.DentistDAO;
+import com.example.efarmoghgiaodontiatrous.dao.Initializer;
+import com.example.efarmoghgiaodontiatrous.dao.ServiceDAO;
+import com.example.efarmoghgiaodontiatrous.dao.SpecializationDAO;
+import com.example.efarmoghgiaodontiatrous.dao.VisitDAO;
+import com.example.efarmoghgiaodontiatrous.domain.Dentist;
+import com.example.efarmoghgiaodontiatrous.memorydao.AppointmentDAOMemory;
+import com.example.efarmoghgiaodontiatrous.memorydao.ClientDAOMemory;
+import com.example.efarmoghgiaodontiatrous.memorydao.DentistDAOMemory;
+import com.example.efarmoghgiaodontiatrous.memorydao.MemoryInitializer;
+import com.example.efarmoghgiaodontiatrous.memorydao.ServiceDAOMemory;
+import com.example.efarmoghgiaodontiatrous.memorydao.SpecializationDAOMemory;
+import com.example.efarmoghgiaodontiatrous.memorydao.VisitDAOMemory;
+import com.example.efarmoghgiaodontiatrous.view.Dentist.ViewStatistics.ViewStatisticsPresenter;
+import com.example.efarmoghgiaodontiatrous.view.Dentist.ViewStatistics.ViewStatisticsView;
+import com.example.efarmoghgiaodontiatrous.view.Dentist.ViewStatistics.ViewStatisticsViewStub;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class DentistViewProfilePresenterTest {
+    DentistViewProfilePresenter presenter;
+    DentistViewProfileView view;
+    private AppointmentDAO appointmentDao;
+    private ClientDAO clientDao;
+    private DentistDAO dentistDao;
+    private ServiceDAO serviceDao;
+    private SpecializationDAO specializationDao;
+    private VisitDAO visitDao;
+
+    @Before
+    public void setUp() {
+        Initializer initializer = new MemoryInitializer();
+        initializer.prepareData();
+
+        appointmentDao = new AppointmentDAOMemory();
+        clientDao = new ClientDAOMemory();
+        dentistDao = new DentistDAOMemory();
+        serviceDao = new ServiceDAOMemory();
+        specializationDao = new SpecializationDAOMemory();
+        visitDao = new VisitDAOMemory();
+
+        view = new DentistViewProfileViewStub();
+        presenter = new DentistViewProfilePresenter(view);
+    }
+
+    @Test
+    public void onShowProfileTest() {
+        Dentist d = dentistDao.find("6");
+        Assert.assertEquals(presenter.onShowProfile("6"), "ID: " + d.getID() + "\n\nName: " + d.getLastName() + " " + d.getFirstName() + "\n\nE-mail: " + d.getEmail() + "\n\nPhone Number: " +
+                d.getTelephoneNo() + "\n\nLicense Number: " + d.getExerciseLicense() + "\n\nUniversity: " + d.getUniversityAttended() +
+                "\n\nLocation: " + d.getInfirmaryLocation().print() + "\n\nYears of Experience: " + d.getTimeOfExperience() +
+                "\n\nServices: " + d.printServices() + "\n\nSpecializations: " + d.printSpecializations());
+
+    }
+
+    @Test
+    public void onUpdateAccountTest() {
+        presenter.onUpdateAccount();
+    }
+
 }
--- app/src/test/java/com/example/efarmoghgiaodontiatrous/view/Dentist/DentistViewProfile/DentistViewProfileViewStub.java
+++ app/src/test/java/com/example/efarmoghgiaodontiatrous/view/Dentist/DentistViewProfile/DentistViewProfileViewStub.java
@@ -1,8 +1,15 @@
 package com.example.efarmoghgiaodontiatrous.view.Dentist.DentistViewProfile;
 
 public class DentistViewProfileViewStub implements DentistViewProfileView {
+
+    public int x=0;
+
     @Override
     public void updateAccount() {
+      this.x++;
+    }
 
+    public int getX(){
+        return x;
     }
 }
